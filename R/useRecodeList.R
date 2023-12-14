4af95f6883aaefe3b0157bfc580ab95eaabeaab1
Nicklas Hafiz <nicklashaf@gmail.com>
2023-12-14 09:47:03 +0100

    Revert "fix error message in useRecodeList, remove newCol argument"
    This reverts commit a29ba237c6447fbaf830a2d9af82dfcc9fa71b40.

--- R/useRecodeList.R
+++ R/useRecodeList.R
@@ -4,22 +4,26 @@
 #'
 #' @param df A data.frame.
 #' @param oldCol Column in `df` which should be recoded.
+#' @param newCol Column name in `df` which should contain the recoded values.
 #' @param recodeList A recode list.
 #'
-#' @return The \code{df} with a recoded \code{newValues}.
+#' @return The \code{df} with a recoded \code{newCol}.
 #'
 #'
 #' @examples
 #' # tbd
 #'
 #' @export
-useRecodeList <- function(df, oldCol, recodeList) {
+useRecodeList <- function(df, oldCol, newCol, recodeList) {
   checkRecodeList(recodeList)
   if (!is.data.frame(df)) stop("'df' must be a data.frame.")
-  if (!(oldCol %in% colnames(df))) stop(paste("The column", oldCol, "is not part of the 'df'."))
+  if (!newCol %in% names(df)) stop("'oldCol' must be a variable in 'df'.")
+  # newCol in df?
 
-  df$newValues <- eatTools::recodeLookup(df[, oldCol], lookup = recodeList)
-  df[!df[, oldCol] %in% recodeList[, 1], "newValues"] <- NA
+  # browser()
 
-  return(df)
+  df[, newCol] <- eatTools::recodeLookup(df[, oldCol], lookup = recodeList)
+  df[!df[, oldCol] %in% recodeList[, 1], newCol] <- NA
+
+  df
 }
