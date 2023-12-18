1fe354af61ba436cb4d159e36af30f587de6bc1f
Nicklas Hafiz <nicklashaf@gmail.com>
2023-12-18 12:10:27 +0100

    rename useRecodeList newColumn arguments

--- tests/testthat/test-useRecodeList.R
+++ tests/testthat/test-useRecodeList.R
@@ -10,8 +10,8 @@ test_that("errors", {
 })
 
 test_that("use recode list", {
-  out <- useRecodeList(df = dat1, oldCol = "var1", recodeList = rl)
-  expect_equal(names(out), c("var1", "var2", "newValues"))
+  out <- useRecodeList(df = dat1, varName = "var1", new_varName = "var_new", recodeList = rl)
+  expect_equal(names(out), c("var1", "var2", "var_new"))
   expect_equal(out[["var1"]], dat1[["var1"]])
-  expect_equal(out[["newValues"]], c(NA, NA, "Germany", "UK", NA, NA))
+  expect_equal(out[["var_new"]], c(NA, NA, "Germany", "UK", NA, NA))
 })
