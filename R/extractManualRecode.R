6520ba5e9266ae29903a3c2743daee2da856795f
Nicklas Hafiz <nicklashaf@gmail.com>
2023-12-18 12:17:24 +0100

    argument for recoded value column in extractManualRecode

--- R/extractManualRecode.R
+++ R/extractManualRecode.R
@@ -1,15 +1,17 @@
 #' Get manual recodes
 #'
-#' @param recodedList A data frame with the automatically recoded values.
+#' @param recodedList A data frame with the automatically recoded values.+
+#' @param varName Character string for the column containing the automatically recoded values.
 #' @return Returns a data frame wich only includes the values that have to be recoded manually. This can be saved to excel for manual recoding.
 #'
 #' @examples # tbd
-extractManualRecode <- function(recodedList) {
+extractManualRecode <- function(recodedList, varName) {
   # Check input object type -------------------------------------------------
   checkmate::assert_data_frame(recodedList)
 
   # Check column names ------------------------------------------------------
-  checkmate::assert_subset(c("newValues"), choices = colnames(recodedList))
+  checkmate::assert_subset(c(varName), choices = colnames(recodedList))
 
-  recodedList[is.na(recodedList$newValues), ]
+  missing_recodes <- recodedList[is.na(recodedList[, varName]), ]
+  return(missing_recodes)
 }
