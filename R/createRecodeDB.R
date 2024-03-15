6b76970513c97dfdacc9ba0543157d0a36383af9
Alina-Apel <apelalin@hu-berlin.de>
2024-03-15 14:38:37 +0100

    update example createRecodeDB

--- R/createRecodeDB.R
+++ R/createRecodeDB.R
@@ -20,12 +20,17 @@
 #'                                   oldValues = c("Baku", "Tokyo", "Kathmandu", "Singapore"),
 #'                                   newValues = c("Azerbaijan", "Japan", "Nepal" , "Singapore")))
 #' recodeListList
-#' filePath_temp <- tempfile(fileext = ".xlsx")
-#' # create the Excel-file
+#' # create csv-file with comma as decimal point (default)
+#' filePath_temp <- tempfile(fileext = ".csv")
+#' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "csv2")
 #' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp)
+#' # create csv-file
+#' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "csv")
+#' # create xlsx-file
+#' filePath_temp <- tempfile(fileext = ".xlsx")
+#' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "xlsx")
 #'
 #' @export
-#'
 createRecodeDB <- function(recodeListList, filePath, fileType = "csv") {
   if (!is.list(recodeListList)) stop("'recodeListList' must be a named list of data.frames.")
   if (!is.data.frame(recodeListList[[1]])) stop("'recodeListList' must be named a list of data.frames.")
@@ -34,14 +39,15 @@ createRecodeDB <- function(recodeListList, filePath, fileType = "csv") {
 
   switch(fileType,
          "xlsx" = {
-           writexl::write_xlsx(recodeListList, path = filePath, col_names = TRUE)}
+           writexl::write_xlsx(recodeListList, path = filePath, col_names = TRUE)},
          "csv" = {
            for(i in 1:length(recodeListList)){
             write.csv(recodeListList[i], file = sub(".csv", paste0("_", names(recodeListList)[i],".csv"), filePath))
-           }}
+           }},
          "csv2" = {
            for(i in 1:length(recodeListList)){
              write.csv2(recodeListList[i], file = sub(".csv", paste0("_", names(recodeListList)[i],".csv"), filePath))
            }})
+
   NULL
 }
