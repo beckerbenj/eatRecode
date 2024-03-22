01096b437e7e9e93b53475f7f6f6e04900c564a4
Alina-Apel <apelalin@hu-berlin.de>
2024-03-22 14:35:11 +0100

    change csv data base output

--- R/createRecodeDB.R
+++ R/createRecodeDB.R
@@ -5,7 +5,8 @@
 #' The `.xlsx` file will have sheets corresponding to each recodeList name.
 #'
 #' @param recodeListList A named list of `recodeLists`.
-#' @param filePath Path to the `.csv` or `.xlsx` file in which the data base should be stored.
+#' @param directory path of the directory where the data base will be saved
+#' @param DBname name of the database (will be used as filename)
 #' @param fileType "csv2" (default), "csv", "xlsx"
 #'
 #' @return NULL
@@ -23,8 +24,9 @@
 #'                                   newValues = c("Azerbaijan", "Japan", "Nepal" , "Singapore")))
 #' recodeListList
 #' # create .csv file with comma as decimal point (default)
-#' filePath_temp <- tempfile(fileext = ".csv")
-#' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "csv2")
+#' directory <- tempdir()
+#' createRecodeDB(recodeListList = recodeListList, directory = directory, DBname = "Countries", fileType = "csv2")
+#'
 #' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp)
 #' # create .csv file
 #' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "csv")
@@ -33,23 +35,37 @@
 #' createRecodeDB(recodeListList = recodeListList, filePath = filePath_temp, fileType = "xlsx")
 #'
 #' @export
-createRecodeDB <- function(recodeListList, filePath, fileType = "csv") {
+createRecodeDB <- function(recodeListList, directory = getwd(), DBname, fileType = "csv") {
+
+  # checks ---------------------------------------------------------------------
   if (!is.list(recodeListList)) stop("'recodeListList' must be a named list of data.frames.")
   if (!is.data.frame(recodeListList[[1]])) stop("'recodeListList' must be named a list of data.frames.")
   if (is.null(names(recodeListList)) || any(is.na(names(recodeListList)))) stop("'recodeListList' must be named a list of data.frames.")
   lapply(recodeListList, checkRecodeList)
 
-  switch(fileType,
-         "xlsx" = {
-           writexl::write_xlsx(recodeListList, path = filePath, col_names = TRUE)},
-         "csv" = {
-           for(i in 1:length(recodeListList)){
-           utils::write.csv(recodeListList[i], file = sub(".csv", paste0("_", names(recodeListList)[i],".csv"), filePath))
-           }},
-         "csv2" = {
-           for(i in 1:length(recodeListList)){
-           utils::write.csv2(recodeListList[i], file = sub(".csv", paste0("_", names(recodeListList)[i],".csv"), filePath))
-           }})
+  # xlsx files -----------------------------------------------------------------
+  if(fileType == "xlsx") {
+    writexl::write_xlsx(recodeListList,
+                        path = paste0(directory,"\\",DBname,".xlsx"),
+                        col_names = TRUE)
+    return(NULL) }
+
+  # csv files ------------------------------------------------------------------
+  dirpath <- file.path(directory,
+                       DBname,
+                       fsep = "\\")
+  unlink(dirpath, recursive = TRUE) # delete existing file ("overwrite")
+  dir.create(dirpath, showWarnings = FALSE) # create data base directory
+
+  if(fileType == "csv") {
+    for(i in 1:length(recodeListList)){
+    utils::write.csv(recodeListList[[i]], file = paste0(dirpath, "\\", names(recodeListList)[i], ".csv"))
+    }
+  } else {
+    for(i in 1:length(recodeListList)){
+      utils::write.csv2(recodeListList[[i]], file = paste0(dirpath, "\\", names(recodeListList)[i], ".csv"))
+    }
+  }
 
   NULL
 }
